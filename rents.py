823af3699856860d9a1880923642a4da2dc87250
J0s3M4rqu3z <32398315+J0s3M4rqu3z@users.noreply.github.com>
2017-09-29 14:02:31 -0400

    Update rents.py

--- rents.py
+++ rents.py
@@ -32,17 +32,3 @@ class rents():
 		print(self.amount)
 		return self.amount
 
-class register_rent():
-	def __init__(self):
-		self.rents_register = []
-
-	def register(self,quantity=1,kinds=['hour']):
-		response = []
-		family = True if (quantity>=3 and quantity<=5) else False
-		for i in range(quantity):
-			self.rents_register.append(rents(kind=kinds[i-1] ,family=family))
-			response.append(len(self.rents_register)-1)
-		return response
-
-	def toCharge(self,register):
-		self.rents_register[register].charge()
